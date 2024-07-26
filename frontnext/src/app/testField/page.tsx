571bd0676db9fabf1ffd99d856bd36b2d235348f
김정수 <kjs20151240@gmail.com>
2024-07-26 16:00:39 +0900

    :bulb: 김정수 : text수정

--- frontnext/src/app/testField/page.tsx
+++ frontnext/src/app/testField/page.tsx
@@ -11,7 +11,10 @@ export default function Home() {
       <main>
         <h1>Welcome to the Page</h1>
         <div className={styles.container}>
-          <Message message="Hellow World" isSuccess={true} />
+          <Message
+            message="조선의 궁궐에 당도한 것을 환영하오, 낯선 이여. 나는 나의 훌륭한 백성들을 굽어살피는 깨우친 임금, 세종이오. "
+            isSuccess={true}
+          />
         </div>
       </main>
     </div>
